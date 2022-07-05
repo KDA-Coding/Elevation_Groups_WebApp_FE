11a2f861b63585734ba977a3c38dfc43f5318842
KDA-Coding <59184838+KDA-Coding@users.noreply.github.com>
2022-07-04 23:03:48 -0400

    Cards behaving acceptably
    Cards have some style and fit in a column on the center of the page. Sufficient for presentation for now.
    
    Moving to turning groups.js array into array filled from Postman API Request

--- elevation_groups_webapp_fe/src/App.js
+++ elevation_groups_webapp_fe/src/App.js
@@ -8,17 +8,26 @@ function App() {
   return (
     <div className="App">
       <input type="text" placeholder="Search for a group!" className="search" />
-      <ul className="groupList">
-        {Groups.map((group) => (
-          
-            <li className="groupListItem" key={group.id.toString()}>
-              <div className="groupCard">
-                <p></p>
-              </div>
-            </li>
-          
-        ))}
-      </ul>
+      <div className="container">
+        <ul className="group-list">
+          {Groups.map((group) => (
+            
+              <li className="group-list-item" key={group.id.toString()}>
+                <div className="card">
+                  <div className="card-header">
+                    <h3>{group.demographic} - {group.group_type}</h3>
+                  </div>
+                  <div className="card-body">
+                      <p>{group.campus} - {group.zip_code}</p>
+                      <p>{group.meeting_date} - {group.description}</p>
+                      <button className="btn">Get Involved</button>
+                  </div>
+                </div>
+              </li>
+            
+          ))}
+        </ul>
+      </div>   
     </div>
   );
 }
